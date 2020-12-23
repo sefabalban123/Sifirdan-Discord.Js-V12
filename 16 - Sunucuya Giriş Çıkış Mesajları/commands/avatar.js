74d99aa8270fcb2fbe0d8863542b4d6dc689ea89
alierenaltindag <47426589+alierenaltindag@users.noreply.github.com>
2020-12-23 04:34:47 -0800

    Sunucuya Giriş Çıkış Mesajları

---
+++ 16 - Sunucuya Giriş Çıkış Mesajları/commands/avatar.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: "avatar",
+    cooldown: 5,
+    guildOnly: true,
+    execute(message, args, client, Embed, Discord){
+
+        const mentionedPlayer = message.mentions.members.first();
+        if(!mentionedPlayer) return message.channel.send(Embed("", "Lütfen Bir Kullanıcıyı Etiketleyiniz.", "info"));
+    
+        const playerAvatar = mentionedPlayer.user.avatarURL({ dynamic: true, format: 'png', size: 256 });
+        
+        if(playerAvatar == null) return message.channel.send(Embed("", `${mentionedPlayer.displayName} Adlı Kişinin Bir Avatarı Yok!`, "error"));
+
+        message.channel.send(playerAvatar);
+    }
+}
