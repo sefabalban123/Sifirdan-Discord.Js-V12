5310d9d659e757f7dd5ae9ce3f2271ce121b9871
alierenaltindag <47426589+alierenaltindag@users.noreply.github.com>
2021-01-02 05:42:21 -0800

    Add files via upload

---
+++ 26 - Yardım Komutu/utils/database.js
@@ -0,0 +1,19 @@
+module.exports = () => {
+
+    const Sequelize = require('sequelize');
+
+    const sequelize = new Sequelize('database', 'user', 'password', {
+        host: 'localhost',
+        dialect: 'sqlite',
+        logging: false,
+        storage: 'database.sqlite'
+    })
+
+    const Tags = sequelize.define('tags', {
+        guild_id: { type: Sequelize.STRING, unique: true, allowNull: false },
+        welcome_message: { type: Sequelize.JSON, defaultValue: {enabled: false, channel_id: "", message: ""} },
+        leave_message: { type: Sequelize.JSON, defaultValue: {enabled: false, channel_id: "", message: ""} }
+    })
+
+    return Tags
+}
