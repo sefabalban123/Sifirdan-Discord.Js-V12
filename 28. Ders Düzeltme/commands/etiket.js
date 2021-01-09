79c1911d2664bd693369e5c274b111ba5dee8a9a
alierenaltindag <47426589+alierenaltindag@users.noreply.github.com>
2021-01-09 03:20:30 -0800

    Add files via upload

---
+++ 28. Ders Düzeltme/commands/etiket.js
@@ -0,0 +1,65 @@
+const { prefix } = require('../config.json');
+
+module.exports = {
+    name: "etiket",
+    description: "Sunucuya Katılan Kullanıcıların Etiketini Düzenler.",
+    cooldown: 3,
+    guildOnly: true,
+    permission: "ADMINISTRATOR",
+    category: "Yetkili",
+    async execute(message, args, Embed, Discord, Tags, tag) {
+
+        const data = tag.get("tag");
+
+        if (args[0] == "aktif") {
+
+            if (!data.tag_name) return message.channel.send(Embed("", "Lütfen Etiket Sistemini Aktifleştirmeden Önce Bir Etiket Ayarlayın.", "error"));
+
+            data.enabled = true;
+            await Tags.update({ tag: data }, { where: { guild_id: message.guild.id } });
+            return message.channel.send(Embed("", "Etiket Sistemi Başarıyla Aktifleştirildi."))
+
+        }
+        else if (args[0] == "pasif") {
+
+            data.enabled = false;
+            await Tags.update({ tag: data }, { where: { guild_id: message.guild.id } });
+            return message.channel.send(Embed("", "Etiket Sistemi Başarıyla Pasifleştirildi."))
+
+        }
+        else if (args[0]) {
+
+            const text = args.join(" ");
+
+            data.tag_name = text
+            await Tags.update({ tag: data }, { where: { guild_id: message.guild.id } });
+            return message.channel.send(Embed("", `Etiket Sistemi Başarıyla **${text.replace("%kullanıcı%", message.member.displayName)}** Olacak Şekilde Ayarlandı.`));
+
+        }
+        else {
+            let isEnabled = false;
+            if (data.enabled) isEnabled = "Aktif"
+            else isEnabled = "Pasif"
+
+            const infoEmbed = new Discord.MessageEmbed()
+                .setTitle("Etiket Sistemi")
+                .setColor("#b6ffa6")
+                .setDescription("Bu Komutu Kullanarak Sunucunuza Yeni Katılan Oyuncuların Etiketlerini Düzenleyebilirsiniz.\n\u200b")
+                .addFields(
+                    { name: `Gereken Yetki:`, value: "Yönetici", inline: true },
+                    { name: `Aktiflik Durumu:`, value: `${isEnabled}\n\u200b`, inline: true },
+                    { name: `${prefix}etiket aktif`, value: "Etiket Sistemini Aktifleştirir" },
+                    { name: `${prefix}etiket pasif`, value: "Etiket Sistemini Pasifleştirir." },
+                    { name: `${prefix}etiket <etiket>`, value: "Sunucuya Yeni Katıaln Oyuncuların Etiketini Belirler.\nBu Kısımda %kullanıcı% Yazdığınzı Kısım Kullanıcının Adı İle Değiştirilir." },
+                )
+
+            message.channel.send(infoEmbed);
+
+        }
+
+    }
+}
+
+// !etiket aktif
+// !etiket pasif
+// !etiket <etiket>
---
+++ 28. Ders Düzeltme/commands/hoşgeldin-mesaj.js
@@ -0,0 +1,66 @@
+const { prefix } = require('../config.json');
+
+module.exports = {
+    name: "hoşgeldin-mesaj",
+    description: "Sunucuya Katılan Oyuncular İçin Hoş\nGeldiniz Mesajı Gönderir.",
+    cooldown: 3,
+    guildOnly: true,
+    permission: "ADMINISTRATOR",
+    category: "Yetkili",
+    aliases: ['hm'],
+    async execute(message, args, Embed, Discord, Tags, tag) {
+        
+        const data = tag.get("welcome_message");
+
+        if(args[0] == "kanal"){
+            const mentionedChannel = message.mentions.channels.first();
+            if(!mentionedChannel) return message.channel.send(Embed("", "Lütfen Bir Kanal Etiketleyiniz.", "info"))
+
+            data.channel_id = mentionedChannel.id;
+
+            await Tags.update({welcome_message: data}, {where: {guild_id: message.guild.id}})
+            return message.channel.send(Embed("", "Hoş Geldiniz Sistemi Kanalı Başarıyla Belirlendi."))
+        }
+        else if(args[0] == "mesaj"){
+            const text = args.splice(1, args.length-1).join(" ");
+            if(!text) return message.channel.send(Embed("", "Lütfen Bir Mesaj Giriniz.", "info"))
+
+            data.message = text;
+
+            await Tags.update({welcome_message: data}, {where: {guild_id: message.guild.id}})
+            return message.channel.send(Embed("", "Hoş Geldiniz Sistemi Mesajı Başarıyla Belirlendi."))
+        }
+        else if(args[0] == "aktif"){
+            data.enabled = true;
+            await Tags.update({welcome_message: data}, {where: {guild_id: message.guild.id}})
+
+            return message.channel.send(Embed("", "Hoşgeldiniz Mesajı Başarıyla Aktifleştirild.i"))
+        }
+        else if(args[0] == "pasif"){
+            data.enabled = false;
+            await Tags.update({welcome_message: data}, {where: {guild_id: message.guild.id}})
+
+            return message.channel.send(Embed("", "Hoşgeldiniz Mesajı Başarıyla Pasifleştirildi."))
+        }
+        else if(args[0] == "test"){
+            message.client.emit("guildMemberAdd", message.member);
+        }
+        else{
+            const infoEmbed = new Discord.MessageEmbed()
+                .setTitle("Hoş Geldiniz Sistemi")
+                .setColor("#b6ffa6")
+                .setDescription("Bu Komutu Kullanarak Hoş Geldiniz Mesajları Oluşturabilirsiniz.\n\u200b")
+                .addFields(
+                    {name: `Gereken Yetki:`, value: "Yönetici", inline: true},
+                    {name: `Aktiflik Durumu:`, value: "Aktif\n\u200b", inline: true},
+                    {name: `${prefix}hoşgeldiniz-mesaj aktif`, value: "Hoş Geldiniz Mesajını Aktifleştirir"},
+                    {name: `${prefix}hoşgeldiniz-mesaj pasif`, value: "Hoş Geldiniz Mesajını Pasifleştirir."},
+                    {name: `${prefix}hoşgeldiniz-mesaj mesaj <mesaj>`, value: "Hoş Geldiniz Mesajını Belirler."},
+                    {name: `${prefix}hoşgeldiniz-mesaj kanal #kanal`, value: "Hoş Geldiniz Mesajı Kanalını Belirler."},
+                    {name: `${prefix}hoşgeldiniz-mesaj test`, value: "Hoş Geldiniz Sistemi Test Mesajını Gönderir."}
+                )
+            
+            message.channel.send(infoEmbed);
+        }
+    }
+}
