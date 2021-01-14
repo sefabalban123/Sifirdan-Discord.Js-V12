8c7f577303d61832f278e0387fad3dd307ebcf46
alierenaltindag <47426589+alierenaltindag@users.noreply.github.com>
2021-01-14 15:20:13 +0300

    Delete 34 - Utils Dinamikleştirme directory

--- 34 - Utils Dinamikleştirme/utils/emojis.js
+++
@@ -1,9 +0,0 @@
-const { owner_guild_id } = require('../config.json')
-
-module.exports = (message, name) => {
-    const ownerGuild = message.client.guilds.cache.get(owner_guild_id);
-    const emojiCol = ownerGuild.emojis.cache.find(emoji => emoji.name == name);
-
-    const emoji = `<:${emojiCol.name}:${emojiCol.id}>`
-    return emoji;
-}
