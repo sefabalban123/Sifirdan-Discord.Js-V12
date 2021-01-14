4ee51168c241d7f8e2a9242fa7dba8c0189eb6ca
alierenaltindag <47426589+alierenaltindag@users.noreply.github.com>
2021-01-14 03:59:51 -0800

    Add files via upload

---
+++ 33 - Bilgi Sistemi/utils/database.js
@@ -0,0 +1,23 @@
+module.exports = () => {
+
+    const Sequelize = require('sequelize');
+
+    const sequelize = new Sequelize('database', 'user', 'password', {
+        host: 'localhost',
+        dialect: 'sqlite',
+        logging: false,
+        storage: 'database.sqlite'
+    })
+
+    const Tags = sequelize.define('tags', {
+        guild_id: { type: Sequelize.STRING, unique: true, allowNull: false },
+        prefix: { type: Sequelize.STRING, defaultValue: "!" },
+        welcome_message: { type: Sequelize.JSON, defaultValue: {enabled: false, channel_id: "", message: ""} },
+        leave_message: { type: Sequelize.JSON, defaultValue: {enabled: false, channel_id: "", message: ""} },
+        tag: {type: Sequelize.JSON, defaultValue: {enabled: false, tag_name: ""}},
+        link_protect_enabled: {type: Sequelize.BOOLEAN, defaultValue: false},
+        auto_role: {type: Sequelize.JSON, defaultValue: {enabled: false, role_id: ""}}
+    })
+
+    return Tags
+}
